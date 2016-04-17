18b5f2910ab3f474d2c1edf0ff888f7cdcabf134
alexoviedo <alexoviedo999@gmail.com>
2016-04-17 02:09:22 -0400

    more styling and logo.

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import Appbar from 'muicss/lib/react/appbar';
 
 const appBarStyle = {
-  backgroundColor:'#00A9E0'
+  backgroundColor:'#00A9E0',
+  padding: '10px 0 10px 40px'
 }
 const Banner = () => (
-  <Appbar style={appBarStyle}/>
+  <div>
+    <Appbar style={appBarStyle}><img src={'../img/logo-white.png'} /></Appbar>
+
+  </div>
 );
 
 export default Banner;
